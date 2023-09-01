3f95262b2b07a9ec2f34d374d14d8c74ab497989
Victoria Danon <victoria.danon@edhec.com>
2023-09-01 23:36:19 +0100

    the streamlit is ugly but cool

--- 1.Home.py
+++ 1.Home.py
@@ -2,37 +2,83 @@ import streamlit as st
 import datetime
 import pandas as pd
 import numpy as np
+import requests
 
 
-# [theme]
+###### Theme and Formatting
 
-# primaryColor="#6eb52f"
-# backgroundColor="#f0f0f5"
-# secondaryBackgroundColor="#e0e0ef"
-# textColor="#262730"
-# font="sans serif"
-
-st.set_page_config(layout="wide")
+st.set_page_config(page_title='TBD', layout="wide")
 
 ## Titles
 
-st.title('London Bike Sharing System')
+st.title('Time to fix all your bike problems')
 
 ### Inputs
-st.subheader('Destination')
 
-st.time_input('I will leave in:', datetime.time(1, 00))
+col1, col2 = st.columns(2)
+
+with col1:
+    st.subheader('Origin')
+    st.text_input('Origin Area','North')
+    origin_neighbourhood=st.text_input('Origin Neighborhood','Hoxton')
+    origin_street=st.text_input('Origin Street','Shoreditch Park')
+
+with col2:
+    st.subheader('Destination')
+    st.text_input('Destination Area','East')
+    destination_neighbourhood=st.text_input('Destination Neighborhood','Liverpool Street')
+    destination_street=st.text_input('Destination Street','Finsbury Circus')
+
+st.subheader('Are you late to work again??')
+
+st.time_input('Yes, I will leave in:', datetime.time(1, 00))
 
 ####
 
+origin=origin_street+", "+origin_neighbourhood
+destination=destination_street+", "+destination_neighbourhood
+
+### Connecting to the API
+
+response= requests.get('https://api.tfl.gov.uk/BikePoint/')
+stations = response.json()
 
 
+col1, col2 = st.columns(2)
 
-### Maps
+for station in stations:
+    if station['commonName']==origin:
+        origin_lat=station['lat']
+        origin_lon=station['lon']
+        for add_property in station['additionalProperties']:
+            if add_property['key'] == 'NbBikes':
+                nb_bikes=add_property['value']
 
-# df = pd.DataFrame(
-#     np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
-#     columns=['lat', 'lon'])
+with col1:
+    st.subheader('Can you find a bike to save your day')
+    st.metric(label="Available bikes", value=nb_bikes)
 
-# st.map(df)
-st.map(latitude='51.5072', longitude='0.1276')
+
+for station in stations:
+    if station['commonName']==destination:
+        destination_lat=station['lat']
+        destination_lon=station['lon']
+        for add_property in station['additionalProperties']:
+            if add_property['key'] == 'NbEmptyDocks':
+                nb_empty_docks=add_property['value']
+
+with col2:
+    st.subheader('BUT Can you drop your bike?')
+    st.metric(label="Empty docks", value=nb_empty_docks)
+
+
+#### Illustrative Map
+
+stations_dict={'Stations': [origin, destination],
+               'Latitude': [origin_lat,destination_lat],
+               'Longitude':[origin_lon,destination_lon]}
+
+
+stations_df=pd.DataFrame(stations_dict)
+
+st.map(data=stations_df,latitude='Latitude',longitude='Longitude',zoom=15)
